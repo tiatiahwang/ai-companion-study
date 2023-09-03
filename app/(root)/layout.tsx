e9245a7da2ca654e906866c9306ed18a5c38781b
tiatiahwang <tiahwang44@gmail.com>
2023-09-02 21:53:08 -0700

    navbar

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import NavBar from '@/components/NavBar';
+
+const RootLayout = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className='h-full'>
+      <NavBar />
+      <main className='md:pl-20 pt-16 h-full'>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default RootLayout;
