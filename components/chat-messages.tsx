c0774d9a1a90d711f437febdc945956053b14c4a
tiatiahwang <tiahwang44@gmail.com>
2023-09-05 12:13:24 -0700

    chat

--- components/chat-messages.tsx
+++ components/chat-messages.tsx
@@ -59,7 +59,7 @@ const ChatMessages = ({
           key={message.content}
           role={message.role}
           content={message.content}
-          src={message.src}
+          src={companion.src}
         />
       ))}
       {isLoading && (
